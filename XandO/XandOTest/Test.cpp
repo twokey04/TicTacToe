4a8f60e8e7a36b1b6a411953cfd07018393b8888
Balc Larisa-Georgiana <larisa.balc@yahoo.ro>
2023-03-19 18:23:22 +0200

    Added TestProject /w @mboboo

---
+++ XandO/XandOTest/Test.cpp
@@ -0,0 +1,29 @@
+#include "Tests.h"
+
+#include "Board.h"
+#include "IGame.h"
+#include "Game.h"
+#include "GameState.h"
+#include "IGameListener.h"
+#include "IPlayer.h"
+#include "Player.h"
+#include "Sign.h"
+
+class MockTest {
+public:
+    MOCK_METHOD(void, SomeMethod, ());
+};
+
+TEST(TestCaseName, TestName) {
+    MockTest mock;
+    EXPECT_CALL(mock, SomeMethod);
+    mock.SomeMethod();
+    EXPECT_EQ(1, 1);
+    EXPECT_TRUE(true);
+}
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
